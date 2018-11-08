a80a6b875ee38a4439f000fc79bf7d601c3d57ca
gvenkata <gvenkata@juniper.net>
2018-11-08 23:01:38 +0530

    Removed white spaces from UDF

--- juniper_official/System/system-processes.py
+++ juniper_official/System/system-processes.py
@@ -9,14 +9,14 @@ def isExceeding(size,res,threshold, **kwargs):
 		size = size * 1024
 	else:
 		size = size[:-1]
-                size = int(size)
+		size = int(size)
 	if res.endswith('M'):
-                res = res[:-1]
-                res = int(res)
-                res = res * 1024
-        else:
-                res = res[:-1]
-                res = int(res)
+		res = res[:-1]
+		res = int(res)
+		res = res * 1024
+	else:
+		res = res[:-1]
+		res = int(res)
 	if (res/size)*100 >= threshold :
 		return True
 	else:
