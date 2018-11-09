9010b6710e9ea1bae3e0a042b47bcd439b389e4b
gvenkata <gvenkata@juniper.net>
2018-11-09 19:34:50 +0530

    Added descriptions and synopsis to all OSPF RCA rules

--- juniper_official/System/system-processes.py
+++ juniper_official/System/system-processes.py
@@ -3,21 +3,21 @@ from __future__ import division
 This function isExceeding returns 'True' if % of size used out of reserved memory is exceeding the threshold
 '''
 def isExceeding(size,res,threshold, **kwargs):
-	if size.endswith('M'):
-		size = size[:-1]
-		size = int(size)
-		size = size * 1024
-	else:
-		size = size[:-1]
-                size = int(size)
-	if res.endswith('M'):
-                res = res[:-1]
-                res = int(res)
-                res = res * 1024
+        if size.endswith('M'):
+            size = size[:-1]
+            size = int(size)
+            size = size * 1024
         else:
-                res = res[:-1]
-                res = int(res)
-	if (res/size)*100 >= threshold :
-		return True
-	else:
-		return False
+            size = size[:-1]
+            size = int(size)
+        if res.endswith('M'):
+            res = res[:-1]
+            res = int(res)
+            res = res * 1024
+        else:
+            res = res[:-1]
+            res = int(res)
+        if (res/size)*100 >= threshold :
+            return True
+        else:
+            return False
