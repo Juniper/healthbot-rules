d6bcc06ea3937d576faa6860afdfb44962ae24bd
gvarao <gvenkata@juniper.net>
2019-01-20 21:02:10 +0530

    Added quotes to regex where square braces been used

---
+++ juniper_official/Linecard/fpc_online.py
@@ -0,0 +1,9 @@
+def run():
+    dev = __proxy__['junos.conn']()
+    op = dev.rpc.get_fpc_information()
+    online = op.xpath('./fpc[normalize-space(state) = "Online"]/slot')
+    ret = []
+    for i in online:
+        if i.text is not None:
+            ret.append({'target': 'fpc'+i.text})
+    return ret
