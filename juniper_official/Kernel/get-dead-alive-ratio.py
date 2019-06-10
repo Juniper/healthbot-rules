df39f1ae4206c40e87485905f3806dfdb63a0a68
PrateekDilip <prateekd@juniper.net>
2019-06-10 15:51:39 +0530

    fix indentation issue in get-dead-alive-ratio.py

--- juniper_official/Kernel/get-dead-alive-ratio.py
+++ juniper_official/Kernel/get-dead-alive-ratio.py
@@ -9,8 +9,8 @@ This function returns the final value of alive/dead ifstates
 
 
 def get_dead_alive_ratio(alive_ifstates_cnt, dead_ifstates_cnt, **kwargs):
-        ratio = 0
-        a = int(alive_ifstates_cnt/2)
+	ratio = 0
+	a = int(alive_ifstates_cnt/2)
 	if (int(dead_ifstates_cnt) > a):
-	    ratio = 1
-        return ratio
+		ratio = 1
+	return ratio
