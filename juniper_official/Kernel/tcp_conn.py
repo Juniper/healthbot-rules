39a8abb55838a2e62c958dcb8e2271aa91c6e8fe
gvarao75@gmail.com <gvenkata@juniper.net>
2019-09-19 22:38:59 +0530

    Removed kernel playbooks and added via other pull request

--- juniper_official/Kernel/tcp_conn.py
+++
@@ -1,7 +0,0 @@
-import re
-
-def run():
-    dev = __proxy__['junos.conn']()
-    op1 = dev.rpc.request_shell_execute(command="sysctl net.inet.tcp.irs_kto")
-    obj1 = re.search('\d+', op1.text)
-    return ({'fields': {"keepalive": int(obj1.group())}})
