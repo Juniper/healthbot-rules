9ba724c01c40334b8481ba8a7b91911b8e951d94
gvarao75@gmail.com <gvenkata@juniper.net>
2019-09-24 18:28:27 +0530

    Enhanced system-processes-memory.rule and system-processes-cpu.rule #199

--- juniper_official/System/system-processes.py
+++ juniper_official/System/system-processes.py
@@ -7,16 +7,26 @@ def isExceeding(size,res,threshold, **kwargs):
             size = size[:-1]
             size = int(size)
             size = size * 1024
+        elif size.endswith('G'):
+            size = size[:-1]
+            size = int(size)
+            size = size * 1024*1024
         else:
             size = size[:-1]
             size = int(size)
+        print(size)
         if res.endswith('M'):
             res = res[:-1]
             res = int(res)
             res = res * 1024
+        elif res.endswith('G'):
+            res = res[:-1]
+            res = int(res)
+            res = res * 1024*1024
         else:
             res = res[:-1]
             res = int(res)
+        print(res)
         if (res/size)*100 >= threshold :
             return True
         else:
