0d6a3d802df44c7bb46ddf380c709803827e7197
gowrisankar <sankarvs@juniper.net>
2020-04-09 19:18:39 +0530

    updated system-processes.py file

--- juniper_official/System/system-processes.py
+++ juniper_official/System/system-processes.py
@@ -1,33 +1,17 @@
 from __future__ import division
 '''
-This function isExceeding returns 'True' if % of size used out of resident memory is exceeding the threshold
+This function converts memory mem string to integer
 '''
-def isExceeding(size,res,threshold, **kwargs):
-        if size.endswith('M'):
-            size = size[:-1]
-            size = int(size)
-            size = size * 1024
-        elif size.endswith('G'):
-            size = size[:-1]
-            size = int(size)
-            size = size * 1024*1024
+def mem_common_form(mem, **kwargs):
+        if mem.endswith('M'):
+            mem = mem[:-1]
+            mem = int(mem)
+            mem = mem * 1024
+        elif mem.endswith('G'):
+            mem = mem[:-1]
+            mem = int(mem)
+            mem = mem * 1024*1024
         else:
-            size = size[:-1]
-            size = int(size)
-        print(size)
-        if res.endswith('M'):
-            res = res[:-1]
-            res = int(res)
-            res = res * 1024
-        elif res.endswith('G'):
-            res = res[:-1]
-            res = int(res)
-            res = res * 1024*1024
-        else:
-            res = res[:-1]
-            res = int(res)
-        print(res)
-        if (res/size)*100 >= threshold :
-            return True
-        else:
-            return False
+            mem = mem[:-1]
+            mem = int(mem)
+        return mem
