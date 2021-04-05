06ea444c689d6dcc49ee6e7921479b18dc8c9612
Anam A <aahmad@juniper.net>
2021-04-05 13:19:12 +0530

    Correcting the facts endpoint

--- juniper_official/System/generic_functions.py
+++ juniper_official/System/generic_functions.py
@@ -366,12 +366,12 @@ def get_device_info(**kwargs):
  
  
 def get_device_info_healthbot(**kwargs):
-    response = requests.get('http://config-server:9000/api/v2/device/%s/facts' % kwargs['device_id'], verify=False)
+    response = requests.get('http://config-server:9000/api/v2/config/device/%s/facts' % kwargs['device_id'], verify=False)
     if response.status_code != 200:
-        response = requests.get('http://config-server:9000/api/v2/device/%s/facts?update=true' % kwargs['device_id'], verify=False)
+        response = requests.get('http://config-server:9000/api/v2/config/device/%s/facts?update=true' % kwargs['device_id'], verify=False)
     device_info = response.json()
     if len(device_info['facts']) == 0:
-        response = requests.get('http://config-server:9000/api/v2/device/%s/facts?update=true' % kwargs['device_id'], verify=False)
+        response = requests.get('http://config-server:9000/api/v2/config/device/%s/facts?update=true' % kwargs['device_id'], verify=False)
         device_info=response.json()
     return device_info
  
