2ea86257ff1f9b6d7f0b6111a6e1df0f8ea95725
vvikramb <vvikramb@juniper.net>
2022-05-04 16:43:01 +0530

    Adding jcloud rules

--- community_supplied/Security/CGNAT/get-float-from-percentage-string.py
+++
@@ -1,9 +0,0 @@
-from __future__ import division
-import re
-'''
-This function returns float value of a percentage string
-'''
-
-def get_float_from_percentage_string(percentage_string, **kwargs):
-    match_value = re.search(r"(\d+\.\d+)\s+\%", percentage_string)
-    return float(match_value.group(1))
--- juniper_official/Chassis/find_ksyncd_cores.py
+++
@@ -1,6 +0,0 @@
-import re
-
-def run():
-    dev = __proxy__['junos.conn']()
-    op1 = dev.rpc.request_shell_execute(command="cli show system core-dumps routing-engine other | grep -c ksyncd")
-    return ({'fields': {"ksyncd_cores": int(op1.text)}})
