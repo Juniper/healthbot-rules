14c571d88648d0e27da0df87bce8bc1a451624d2
vvikramb <vvikramb@juniper.net>
2022-05-24 11:17:29 +0530

    Adding to jcloud branch

--- community_supplied/Protocols/Bgp/BGPneighbor.py
+++
@@ -1,5 +0,0 @@
-def neighbor(neighbor_list,peer_state, **kwargs):
-    if peer_state in neighbor_list:
-        return 1
-    else:
-        return 0
--- juniper_official/System/system-sensors.py
+++
@@ -1,6 +0,0 @@
-from __future__ import division
-'''
-This function subtract returns difference between a and b
-'''
-def subtract(threshold, **kwargs):
-        return 100-int(threshold)
